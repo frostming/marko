9ff641795cf00e9685c438ec025a189e66130039
Frost Ming <mianghong@gmail.com>
2018-09-11 17:09:47 +0800

    fix pangu delimiters

--- marko/ext/pangu.py
+++ marko/ext/pangu.py
@@ -26,7 +26,7 @@ import re
 CJK_RE = (r'\u2e80-\u2eff\u2f00-\u2fdf\u3040-\u309f\u30a0-\u30ff\u3100-\u312f'
           r'\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff')
 PANGU_RE = re.compile(
-    r'((?<=[{cjk}])(?=[^\n{cjk}])|(?<=[^\n{cjk}])(?=[{cjk}]))'
+    r'((?<=[{cjk}])(?=[^\s{cjk}])|(?<=[^\s{cjk}])(?=[{cjk}]))'
     .format(cjk=CJK_RE)
 )
 
