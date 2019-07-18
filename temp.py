2a9d5c56ba4104a9bb99f3917cd1aeaedab43f69
Frost Ming <mianghong@gmail.com>
2019-07-18 21:24:22 +0800

    Remove useless file

--- temp.py
+++
@@ -1,7 +0,0 @@
-from marko.ext.gfm import GFMarkdown
-from marko import Markdown
-from marko.ast_renderer import ASTRenderer
-
-a = '<blockquote>\n  <xmp> is disallowed.  <XMP> is also disallowed.\n</blockquote>\n'
-
-print(GFMarkdown()(a))
