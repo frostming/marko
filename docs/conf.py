ce4e7890f7dbff421e4b0e900096fff2680c965f
Frost Ming <mianghong@gmail.com>
2019-08-02 20:27:17 +0800

    add github banner

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,11 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_banner': True,
+    'github_user': 'frostming',
+    'github_repo': 'marko'
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
