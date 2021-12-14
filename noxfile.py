8c0141e11b7b6b19c0089c662711590c4f9dc420
Frost Ming <mianghong@gmail.com>
2021-12-14 18:29:03 +0800

    Run CI on py310

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,7 @@ import os
 os.environ.update(PDM_IGNORE_SAVED_PYTHON="1")
 
 
-@nox.session(python=["3.6", "3.8", "3.9"])
+@nox.session(python=["3.6", "3.8", "3.9", "3.10"])
 def tests(session):
     session.run("pdm", "install", "-d", external=True)
     session.run("pytest", "tests/")
